11bed4e2aab33bc537470b059d005eca3d538fbd
PauliusK-bit <kareiva.paulius13@gmail.com>
2025-05-21 20:49:58 +0300

    Created Project Roots

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const process = require("process");
+require("dotenv").config();
+require("./db");
+
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+
+app.set("view engine", "ejs");
+app.set("views", path.join("views"));
+
+app.use(express.static("public"));
+
+app.locals.siteTitle = "My Website";
+app.locals.currentDate = new Date().getFullYear();
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const PORT = process.env.PORT || 3008;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}.`));
